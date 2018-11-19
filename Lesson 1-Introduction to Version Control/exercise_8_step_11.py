37351f4ecd66afb4069350aeaf5f60327ec6e236
Alex Magana <gatobumalex@gmail.com>
2018-11-20 00:14:42 +0300

    Add code bundle for exercise 8 step 11

---
+++ Lesson 1-Introduction to Version Control/exercise_8_step_11.py
@@ -0,0 +1,18 @@
+#Exercise 8: Creating a Repository in a Local Environment
+
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def multiply(self):
+        pass
