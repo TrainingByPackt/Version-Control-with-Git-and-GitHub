72616f90283878198480bcb5ccb0e34cecbd3a72
Alex Magana <gatobumalex@gmail.com>
2018-11-20 08:36:22 +0300

    Initialize operators and operands

---
+++ Lesson 2-Versioning Commits/exercise_1_step_6.py
@@ -0,0 +1,18 @@
+# Exercise 1: Viewing and Establishing the Status of a File
+
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def multiply(self):
+        pass
